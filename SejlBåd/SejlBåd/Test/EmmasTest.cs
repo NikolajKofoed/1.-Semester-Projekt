69422725336cd8203b648390da9d9e6e0e466486
Emma Hyttel <emhyttel@gmail.com>
2023-11-24 12:41:49 +0100

    EmmasTest class

---
+++ SejlBåd/SejlBåd/Test/EmmasTest.cs
@@ -0,0 +1,16 @@
+﻿namespace SejlBåd.Test
+{
+    public class EmmasTest
+    {
+        public int MyProperty { get; set; }
+
+        public EmmasTest()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(MyProperty)}={MyProperty.ToString()}}}";
+        }
+    }
+}
