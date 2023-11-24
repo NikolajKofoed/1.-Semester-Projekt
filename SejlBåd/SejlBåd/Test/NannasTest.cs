890f1211179f9a7b1fc14d272c580df95e471d50
NannaRister <nannarister@hotmail.com>
2023-11-24 12:46:29 +0100

    Nannas Test

---
+++ SejlBåd/SejlBåd/Test/NannasTest.cs
@@ -0,0 +1,16 @@
+﻿namespace SejlBåd.Test
+{
+    public class NannasTest
+    {
+        public int MyProperty { get; set; }
+
+        public NannasTest()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(MyProperty)}={MyProperty.ToString()}}}";
+        }
+    }
+}
