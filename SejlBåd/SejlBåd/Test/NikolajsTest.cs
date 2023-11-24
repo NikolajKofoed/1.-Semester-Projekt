bf1c8d62a6170f28d10d6fa29b7119be1148c146
NikolajKofoed <nik003@edu.zealand.dk>
2023-11-24 12:35:50 +0100

    test
    min test

---
+++ SejlBåd/SejlBåd/Test/NikolajsTest.cs
@@ -0,0 +1,18 @@
+﻿namespace SejlBåd.Test
+{
+    public class NikolajsTest
+    {
+        public int MyProperty { get; set; }     
+
+        public NikolajsTest()
+        {
+
+        }
+
+
+        public static void print()
+        {
+            Console.WriteLine("Hi There!");
+        }
+    }
+}
