212712552bdafa2802027c003bee89b814f30cde
NannaRister <nannarister@hotmail.com>
2023-11-30 13:20:20 +0100

    Mockdata klasse, sailingclass klasse, interface og razor page

---
+++ SejlBåd/SejlBåd/Models/SailingClass.cs
@@ -0,0 +1,28 @@
+﻿
+namespace SejlBåd.Models
+{
+    public class SailingClass
+    {
+        public List<Member> ClassMembers { get; set; }
+        public string ClassName { get; set; }
+        public string ClassDate { get; set; }
+        public int ClassSize { get; set; }
+        public string ClassDetails { get; set; }
+
+        public SailingClass(string className, string classDate, string classDetails)
+        {
+            ClassName = className;
+            ClassDate = classDate;
+            ClassDetails = classDetails;
+        }
+
+        public SailingClass()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(ClassName)}={ClassName}, {nameof(ClassDate)}={ClassDate.ToString()}, {nameof(ClassSize)}={ClassSize.ToString()}, {nameof(ClassDetails)}={ClassDetails}}}";
+        }
+    }
+}
