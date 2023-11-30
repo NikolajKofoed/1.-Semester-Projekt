adaec41f81ed603a8143238532dfa3f85c754b92
MissMaaai <152159186+MissMaaai@users.noreply.github.com>
2023-11-30 13:47:45 +0100

    addboatsservice og razorpageboat

--- SejlBåd/SejlBåd/Program.cs
+++ SejlBåd/SejlBåd/Program.cs
@@ -1,3 +1,4 @@
+using SejlBåd.Services.BoatService;
 using SejlBåd.Services.MemberServices;
 using SejlBåd.Services.WeatherServices;
 
@@ -9,6 +10,7 @@ builder.Services.AddHttpClient();
 builder.Services.AddSingleton<IMemberService, MemberService>();
 builder.Services.AddTransient<JsonFileMemberService>();
 builder.Services.AddTransient<IWeatherService, WeatherService>();
+builder.Services.AddSingleton<IBoatService, BoatService>();
 
 var app = builder.Build();
 
---
+++ SejlBåd/SejlBåd/Services/BoatService/BoatService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Routing.Constraints;
+using SejlBåd.Models;
+
+namespace SejlBåd.Services.BoatService
+{
+    public class BoatService : IBoatService
+    {
+        List<Boat> boats = new List<Boat>();
+
+        void IBoatService.AddBoats(Boat boat)
+        {
+            boats.Add(boat);
+        }
+
+        Boat IBoatService.GetBoats()
+        {
+
+            foreach (var boat in boats)
+            {
+                if ( boat.Length == 200)
+                {
+                    return boat;
+                }
+            }
+            return null;
+        }
+        public List<Boat> listOfBoats()
+        {
+            return boats;
+        }
+
+        Boat? IBoatService.LookUpBoat(int Id)
+        {
+            foreach (var boat in boats)
+            {
+                if (boat.Id == Id)
+                {
+                    return boat;
+                }
+            }
+            return null;
+        }
+
+        void IBoatService.RemoveBoats(Boat boat)
+
+        {
+           foreach (Boat b in boats)
+            {
+                if (boat == b)
+                {
+                    boats.Remove(b);
+                    break;
+                }
+            }
+        }
+    }
+}
