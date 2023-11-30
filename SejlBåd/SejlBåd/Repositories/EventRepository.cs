7d0c83c465bdcd3ab67ca2a953e5d9a69dd5cf7d
Alexander Schmidt <als002@edu.zealand.dk>
2023-11-30 18:25:57 +0100

    Jeg har tilføjet mappen EventServices og lavet metoderne til EventRepository - Dog ikke testet endnu.

--- SejlBåd/SejlBåd/Repositories/EventRepository.cs
+++ SejlBåd/SejlBåd/Repositories/EventRepository.cs
@@ -2,6 +2,40 @@
 {
     public class EventRepository
     {
-        List<Models.Event> Event;
+        List<Models.Event> EventList = new List<Models.Event>();
+
+        public void AddEvent(string eventName, string eventDetails, string eventDate)
+        {
+            EventList.Add(new Models.Event { EventName = eventName, EventDetails = eventDetails, EventDate = eventDate });
+        }
+
+        public void RemoveEvent(string eventName)
+        {
+            Models.Event EventToRemove = EventList.FirstOrDefault(e => e.EventName == eventName);
+            if (EventToRemove != null)
+            {
+                EventList.Remove(EventToRemove);
+            }
+        }
+
+        public void UpdateEvent(string eventName, string eventDetails, string eventDate)
+        {
+            Models.Event EventToUpdate = EventList.FirstOrDefault();
+            if (EventToUpdate != null)
+            {
+                EventToUpdate.EventName = eventName;
+                EventToUpdate.EventDetails = eventDetails;
+                EventToUpdate.EventDate = eventDate;
+            }
+        }
+
+        public void DeleteEvent(string eventName)
+        {
+            Models.Event EventToDelete = EventList.FirstOrDefault();
+            if (EventToDelete != null)
+            {
+                EventList.Remove(EventToDelete);
+            }
+        }
     }
 }
