2065d266f7d9a0dfd8f414cb2bfd5a1c5e2b41f1
alsvike <148956728+alsvike@users.noreply.github.com>
2023-11-30 13:23:39 +0100

    Jeg har lavet EvenRepo, og tilføjet metoder

--- SejlBåd/SejlBåd/Repositories/EventRepository.cs
+++ SejlBåd/SejlBåd/Repositories/EventRepository.cs
@@ -2,6 +2,22 @@
 {
     public class EventRepository
     {
-        List<Models.Event> Event;
+        List<Models.Event> EventRepo = new List<Models.Event>();
+
+        public void AddEvent()
+        {
+        }
+
+        public void RemoveEvent()
+        {
+        }
+
+        public void EditEvent()
+        {
+        }
+
+        public void DeleteEvent()
+        {
+        }
     }
 }
---
+++ SejlBåd/SejlBåd/Services/EventServices/EventService.cs
@@ -0,0 +1,6 @@
+﻿namespace SejlBåd.Services.EventServices
+{
+    public interface EventService
+    {
+    }
+}
