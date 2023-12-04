45fb778a36908ab2bffc4689ccae98d864c9ae52
NikolajKofoed <nik003@edu.zealand.dk>
2023-12-04 17:11:28 +0100

    started styling dock

--- SejlBåd/SejlBåd/Pages/DockSpotPages/RentDockSpot.cshtml.cs
+++ SejlBåd/SejlBåd/Pages/DockSpotPages/RentDockSpot.cshtml.cs
@@ -12,7 +12,7 @@ namespace SejlBåd.Pages.DockSpotPages
         [BindProperty] public DockSpot DockSpot { get; set; }
         [BindProperty] public DateTime RentPeriodStart { get; set; }
         [BindProperty] public DateTime RentPeriodEnd { get; set; }
-        [BindProperty] public string CardType { get; set; }
+        [BindProperty] public CreditCard CreditCard{ get; set; }
         
 
         public RentDockSpotModel(IDockSpotService dockSpotService)
