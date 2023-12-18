039cd198a2a3bdfe69c2e3ae9eb5d058efd34a0f
MissMaaai <152159186+MissMaaai@users.noreply.github.com>
2023-12-18 14:25:37 +0100

    ændringer

--- SejlBåd/SejlBåd/Pages/BoatPages/DeleteBoat.cshtml.cs
+++ SejlBåd/SejlBåd/Pages/BoatPages/DeleteBoat.cshtml.cs
@@ -9,7 +9,7 @@ namespace SejlBåd.Pages.BoatPages
     {
         private Services.BoatService.IBoatService _boatService;
         [BindProperty]
-        public Models.Boat boatModel { get; set; }
+        public Models.Boat Boat { get; set; }
 
         public DeleteBoatModel(IBoatService BoatService)
         {
@@ -17,16 +17,14 @@ namespace SejlBåd.Pages.BoatPages
         }
         public IActionResult OnGet(int id)
         {
-            if (boatModel == null)
-            {
-                return RedirectToPage("Junior");
-            }
+            Boat = _boatService.GetBoat(id);
+            
             return Page();
         }
 
         public IActionResult OnPost(int id)
         {
-            boatModel = _boatService.GetBoat(id);
+            Boat = _boatService.GetBoat(id);
             Models.Boat DeleteBoat = _boatService.DeleteBoat(id);
             if (DeleteBoat == null)
             {
