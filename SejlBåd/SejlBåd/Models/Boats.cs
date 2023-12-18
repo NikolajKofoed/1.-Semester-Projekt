0a3da5062003012af096f2f506ed74e2c639dac1
Nikolaj Kofoed <nik003@edu.zealand.dk>
2023-12-18 09:51:00 +0100

    boats

---
+++ SejlBåd/SejlBåd/Models/Boats.cs
@@ -0,0 +1,21 @@
+﻿namespace SejlBåd.Models
+{
+    public class Boats
+    {
+        private int nextId = 1; 
+        public int Id { get; set; }
+        public List<Boat> BoatList { get; set; }
+        public string Navn { get; set; }
+
+        public Boats(List<Boat> boatList, string navn)
+        {
+            Id = nextId++;
+            BoatList = boatList;
+            Navn = navn;
+        }
+
+        public Boats()
+        {
+        }
+    }
+}
